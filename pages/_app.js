a50d32d3f8dd30554b9db249f4343ae5e95e27e5
Theodor Vararu <theo@vararu.org>
2019-10-06 17:38:56 +0100

    Set title to blog.vararu.org

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import App from "next/app";
+import Head from "next/head";
 
 export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <>
+        <Head>
+          <title>blog.vararu.org</title>
+        </Head>
         <h1>
           <a href="/">blog.vararu.org</a>
         </h1>
