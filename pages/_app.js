fd0911858ef16e66f7da5725cdf7726920c29c28
Theodor Vararu <theo@vararu.org>
2019-10-06 17:59:35 +0100

    Improve overall styling

--- pages/_app.js
+++ pages/_app.js
@@ -9,10 +9,44 @@ export default class MyApp extends App {
         <Head>
           <title>blog.vararu.org</title>
         </Head>
-        <h1>
-          <a href="/">blog.vararu.org</a>
-        </h1>
-        <Component {...pageProps} />
+        <style global jsx>{`
+          body {
+            font-family: Charter, Iowan, Palatino, "Times New Roman", Times,
+              serif;
+            margin: 1rem;
+          }
+
+          main {
+            margin: 0 auto;
+            max-width: 40rem;
+          }
+
+          a {
+            color: #273c75;
+          }
+
+          a:visited {
+            color: #192a56;
+          }
+
+          ul {
+            padding-left: 1.25rem;
+          }
+
+          pre {
+            overflow-y: scroll;
+            background: #2f3640;
+            padding: 1rem;
+            color: #ffffff;
+            margin: 0 -1rem;
+          }
+        `}</style>
+        <main>
+          <h1>
+            <a href="/">blog.vararu.org</a>
+          </h1>
+          <Component {...pageProps} />
+        </main>
       </>
     );
   }
